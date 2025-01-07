668e9fa221070f565f07b6f8958d817c00043748
Emily Garcia <kristin.fullstackdev@gmail.com>
2025-01-07 16:51:45 -0300

    Update Cad_produtos.sql

--- Cad_produtos.sql
+++ Cad_produtos.sql
@@ -1,138 +1,27 @@
-SELECT DISTINCT
-    T0."ItemCode",
-    T0."ItemName",
-    T6."UpdateDate" AS "Última modificação",
-      
-    CASE
-        WHEN T0."validFor" = 'Y' THEN 'Sim'
-        ELSE 'Não'
-    END AS "Ativo",
-    
-    CASE
-        WHEN T0."U_CbxForaLinha" = '0' THEN 'Sim'
-        WHEN T0."U_CbxForaLinha" = '1' THEN 'Não'
-        ELSE ''
-    END AS "Fora de Linha",
+SELECT T0."CardCode"
+	,T0."CardName"
+	,T0."AliasName"
+	,T0."E_Mail"
+	,T0."SlpCode"
+	,T0."HousBnkAct"
+	,T0."GroupNum"
+	,T0."GroupCode"
+	,T0."ProjectCod"
+	,T0."ShipType"
+    ,T0."SlpCode"
+	,T2."AddrType"
+	,T2."Street"
+	,T2."StreetNo" 
+	,T2."Block"
+	,T2."State"
+	,T2."City"
+	,T2."Country"
+	,T2."Building"
+	,T0."U_TaktTime"
+	,T0."U_TransitTime"
+    ,T0."CardCode" || ' - ' || T0."CardName" As "Fornecedor"
 
-    T0."U_LicImport",
-    T0."U_Dumping",
-
-    CASE
-        WHEN T0."SellItem" = 'Y' THEN 'Sim'
-        ELSE 'Não'
-    END AS "Item de Venda",
-
-    CASE
-        WHEN T0. "U_ItemMacro" = 0 THEN 'Artesanato'
-        WHEN T0. "U_ItemMacro" = 1 THEN 'Colas & Fitas'
-        WHEN T0. "U_ItemMacro" = 2 THEN 'Cortes & Réguas'
-        WHEN T0. "U_ItemMacro" = 3 THEN 'Costurados'
-        WHEN T0. "U_ItemMacro" = 4 THEN 'Escrita & Pintura'
-        WHEN T0. "U_ItemMacro" = 5 THEN 'Papéis'
-        WHEN T0. "U_ItemMacro" = 6 THEN 'Suprimentos'
-        WHEN T0. "U_ItemMacro" = 7 THEN 'Pedagógicos'
-        ELSE ''
-    END AS "Macrocategoria",
-
-    T1."ItmsGrpNam",
-    T0."U_CVA_Desc_SubGrupo",
-    T0."U_Tx_Linha",
-    T0."U_Tx_Colecao",
-    T0."U_ItemSubstituto",
-    T0."U_MotivoSubstituicao",
-
-    -- SKU
-    CASE
-        WHEN T0."BuyUnitMsr" = 'Pacote' THEN 'PCT'
-        WHEN T0."BuyUnitMsr" = 'Caixa' THEN 'CX'
-        WHEN T0."BuyUnitMsr" = 'Unidade' THEN 'UN'
-        WHEN T0."BuyUnitMsr" = 'Tubo' THEN 'TUB'
-        WHEN T0."BuyUnitMsr" = 'Pote' THEN 'POT'
-        WHEN T0."BuyUnitMsr" = 'Kilograma' THEN 'KG'
-        ELSE ''
-    END AS "SKU",
-
-    T0."U_TX_UTribEx",
-    T0."U_TX_FtTribEx",
-    T2."NcmCode",
-    T0."U_TX_CodigoCest",
-    T4."U_IPI_VALUE",
-
-    CASE
-        WHEN T0."ProductSrc" = '1' THEN 'Importado'
-        WHEN T0."ProductSrc" = '2' THEN 'Importado'
-        WHEN T0."ProductSrc" = '6' THEN 'Importado'
-        ELSE 'Nacional'
-    END AS "Origem",
-
-    CASE
-        WHEN T3."ISOriCntry" = 'CN' THEN 'China'
-        WHEN T3."ISOriCntry" = 'BR' THEN 'Brasil'
-        WHEN T3."ISOriCntry" = 'IN' THEN 'Índia'
-        WHEN T3."ISOriCntry" = 'PY' THEN 'Paraguai'
-        ELSE T3."ISOriCntry"
-    END AS "País de Origem",
-
-    T0."U_NumInmetro",
-
-    CASE
-        WHEN T0."U_NumInmetro" IS NULL OR T0."U_NumInmetro" = '' OR T0."U_NumInmetro" = '000000/0000'
-        OR T0."U_NumInmetro" = 'N/A' OR T0."U_NumInmetro" = '0' THEN 'Não'
-        ELSE 'Sim'
-    END AS "Inmetro",
-    
-    T0."U_ItemComposicao",
-
-    -- Códigos de barras separados
-    T0."U_InnerCodBarras",
-    T0."U_MasterCodBarras",
-    T0."U_ProdCodBarras",
-    T0."U_EmbCodBarras",
-
-    T0."BVolume" AS "Volume (CUBAGEM)",
-    T0."BWeight1" AS "Peso de Compra (PESO LÍQUIDO)",
-    T0."SWeight1" AS "Peso de Venda (PESO BRUTO)"
-    
-FROM OITM T0
-/*LEFT JOIN OITB T1
-    ON T0."ItmsGrpCod" = T1."ItmsGrpCod"
-LEFT JOIN ITM10 T3
-    ON T0."CountryOrg" = T3."ISOriCntry"
-LEFT JOIN ONCM T2
-    ON T0."NCMCode" = T2."AbsEntry"
-LEFT JOIN "@BRW_DADOS_NCM_LINHA" T4
-    ON T2."NcmCode" = T4."Code"
-LEFT JOIN OBCD T5
-    ON T0."ItemCode" = T5."ItemCode"
-LEFT JOIN AITM T6
-    ON T0."ItemCode" = T6."ItemCode"*/
-LEFT JOIN OITB T1
-    ON T0."ItmsGrpCod" = T1."ItmsGrpCod"
-LEFT JOIN ITM10 T3
-    ON T0."CountryOrg" = T3."ISOriCntry"
-LEFT JOIN ONCM T2
-    ON T0."NCMCode" = T2."AbsEntry"
-LEFT JOIN "@BRW_DADOS_NCM_LINHA" T4
-    ON T2."NcmCode" = T4."Code"
-LEFT JOIN OBCD T5
-    ON T0."ItemCode" = T5."ItemCode"
-LEFT JOIN (
-    SELECT "ItemCode", MAX("UpdateDate") AS "UpdateDate"
-    FROM AITM
-    GROUP BY "ItemCode"
-) T6
-    ON T0."ItemCode" = T6."ItemCode"
-WHERE
-    T0."MatType" = 0
-    AND T0."ItemCode" NOT IN ('3229', 'ITNOVO')
-    AND T0."validFor" >= [%0]  
-/*GROUP BY
-    T0."ItemCode", T0."validFor", T0."U_CbxForaLinha", T0."SellItem", T0."U_Tx_Linha", 
-    T0."U_Tx_Colecao", T1."ItmsGrpNam", T0."U_CVA_Desc_SubGrupo", T0."ItemName",
-    T0."U_ItemSubstituto", T0."U_MotivoSubstituicao", T0."BuyUnitMsr", T0."U_TX_UTribEx",
-    T0."U_TX_FtTribEx", T2."NcmCode", T0."U_TX_CodigoCest", T4."U_IPI_VALUE", 
-    T0."ProductSrc", T3."ISOriCntry", T0."U_NumInmetro", T0."U_ItemComposicao", 
-    T0."BVolume", T0."BWeight1", T0."SWeight1", T0. "U_ItemMacro",     T0."U_InnerCodBarras",
-    T0."U_MasterCodBarras", T0."U_ProdCodBarras", T0."U_EmbCodBarras", T6."UpdateDate"*/
-ORDER BY
-    T0."ItemCode"
+FROM "SUA_TABELA"."OCRD" T0
+INNER JOIN "SUA_TABELA"."CRD1" T2 ON T0."CardCode" = T2."CardCode"
+AND T2."Address" = T0."ShipToDef"
+WHERE T0."GroupCode" = '106' OR  T0."GroupCode" = '121'
